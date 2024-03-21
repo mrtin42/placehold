e127a1747e16e36f3fe31cff6883118d0ff4c72a
martin <martin@tub3.uk>
2024-03-21 22:43:01 +0000

    Update font size and logo size in og/route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,8 +1,11 @@
 import type { NextRequest } from 'next/server'
 import { getRequestContext } from '@cloudflare/next-on-pages'
 import { ImageResponse } from 'next/og'
+import { Inter } from 'next/font/google'
 
 export const runtime = 'edge'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export async function GET(request: NextRequest) {
   const fontData = await fetch('https://hackedfont.com/HACKED.ttf').then((res) => res.arrayBuffer())
@@ -10,16 +13,16 @@ export async function GET(request: NextRequest) {
   return new ImageResponse(
     (
       <div style={{ backgroundColor: 'black', color: 'white', padding: '1rem', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '100%', height: '100%'}}>
-          <img src="https://placehold.pages.dev/logo.png" alt="logo" style={{ width: '200px', height: '200px' }} />
-          <h1 style={{ fontFamily: 'HACKED', fontSize: '2rem', marginLeft: '1rem' }}>{hostname}</h1>
-          <p style={{ fontSize: '1rem', marginLeft: '1rem' }}>Parked by MartinDEV</p>
+          <img src="https://placehold.pages.dev/logo.png" alt="logo" style={{ width: '350px', height: '350px' }} />
+          <h1 style={{ fontFamily: 'HACKED', fontSize: '9rem', }}>{hostname}</h1>
       </div>
     ),
     {
       width: 1200,
       height: 630,
       fonts: [
-        { name: 'HACKED', data: fontData, style: 'normal'}
+        { name: 'HACKED', data: fontData, style: 'normal'},
+
       ]
     }
   );
